19f21a5aa1521fcdd1b26ca3b3c7dd741ec877c3
emat68 <emalvarezt@hotmail.com>
2019-09-25 13:05:42 -0300

    cales2

---
+++ cliente.js
@@ -0,0 +1,67 @@
+var arrclientes = [];
+function callobjetoCliente()
+{
+
+//Definicion de objetos dentro de objetos
+var cliente= {
+    idcliente: 1,
+    nombre: "Ernesto",
+    fechacreacion: "2019-09-20",
+    direccion: {
+        calle: "Pepe Viña",
+        numero: "187",
+        comuna: "La Reina",
+        region: "metropolitana"
+    }
+}
+arrclientes.push(cliente);
+
+var cliente2= {
+    idcliente: 2,
+    nombre: "Jose",
+    fechacreacion: "2019-09-21",
+    direccion: {
+        calle: "Pepe Viña",
+        numero: "187",
+        comuna: "La Reina",
+        region: "metropolitana"
+    }
+}
+arrclientes.push(cliente2);
+var cliente3= {
+    idcliente: 3,
+    nombre: "Jhon",
+    fechacreacion: "2019-09-22",
+    direccion: {
+        calle: "Pepe Viña",
+        numero: "187",
+        comuna: "La Reina",
+        region: "metropolitana"
+    }
+}
+arrclientes.push(cliente3);
+
+};
+
+(function recorre1(){
+    callobjetoCliente();
+    var candidatos = [];
+    candidatos.push(arrclientes[0]);
+   
+   //Esta función permite comparar dos variables, el resulatado lo deja en candidatos
+   //luego toma el siguiente registro de arrclientes y lo compara con el resultado
+   const primero = arrclientes.reduce(function(arrclientes,candidatos)
+   {return arrclientes.fechacreacion > candidatos.fechacreacion? arrclientes:candidatos
+}
+   );
+   console.log("Usuario mas Nuevo =>>")
+   console.log( primero);
+ 
+   const ultimo = arrclientes.reduce(function(arrclientes,candidatos)
+   {return arrclientes.fechacreacion < candidatos.fechacreacion? arrclientes:candidatos
+}
+   );
+   console.log("Usuario mas viejo =>>")
+   console.log( ultimo);
+})();
+
---
+++ numprimos.js
@@ -0,0 +1,34 @@
+(function(){
+  var j = 1;
+  var numerosPrimos = []; 
+
+
+let intervalo = setInterval(numerosprimos,500);
+ setTimeout(()=>{clearInterval(intervalo)},5000);
+
+function numerosprimos(){
+   
+    if (primo(j)) {
+      numerosPrimos.push(j);
+    }
+    j++;
+    console.log("Numeros Primos" + numerosPrimos);
+
+};
+
+function primo(numero) {
+
+    for (var i = 2; i < numero; i++) {
+  
+      if (numero % i === 0) {
+        return false;
+      }
+  
+    }
+  
+    return numero !== 1;
+  }
+
+})();
+
+   
