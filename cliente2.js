228906bdaf0e5da0970400fd307650874ca17920
emat68 <emalvarezt@hotmail.com>
2019-09-27 13:26:35 -0400

    curso3

--- cliente2.js
+++ cliente2.js
@@ -1,17 +1,45 @@
-var arrclientes = [];
-function callobjetoCliente(P_id,P_nombre,P_fecha,P_calle,P_numero,P_comuna,P_region)
-{
+var arrclientes2 = [];
 
-//Definicion de objetos dentro de objetos
-var cliente= {
-    idcliente: @id,
-    nombre: ,
-    fechacreacion: "2019-09-20",
-    direccion: {
-        calle: "Pepe Viña",
-        numero: "187",
-        comuna: "La Reina",
-        region: "metropolitana"
+class Direccion{
+    constructor(_calle,_numero,_comuna,_region){
+        this.calle = _calle;
+        this.numero = _numero;
+        this.comuna = _comuna;
+        this.region = _region;
     }
 }
-arrclientes.push(cliente);
+
+class Cliente {
+    constructor(P_id,P_nombre,P_fecha,P_calle,P_numero,P_comuna,P_region)
+    {   
+        this.idcliente = P_id;
+        this.nombre = P_nombre;
+        this.fechacreacion = P_fecha;
+        this.direccion = new Direccion(P_calle,P_numero ,P_comuna, P_region);
+    }
+
+    arrclientes2.push(n1_cliente);
+
+    getnombres2() {
+        return arrclientes2;
+    }
+}
+
+
+//let n1_cliente = new Cliente("1","Ernesto","2019-09-22","Pepe Vila","187","La Reina","13");
+//let n2_cliente = new Cliente("1","juan","2019-09-24","Pepe Vila","187","La Reina","13");
+//let n3_cliente = new Cliente("1","Pedro","2019-09-27","Pepe Vila","187","La Reina","13");
+//arrclientes2.push(n1_cliente);
+//arrclientes2.push(n2_cliente);
+//arrclientes2.push(n3_cliente);
+
+// var getnombres2 = function()
+//   {
+//     return arrclientes2;
+// };
+
+
+module.exports = { 
+    cli2:getnombres2
+}
+
--- curso3_2.js
+++ curso3_2.js
@@ -1,8 +1,16 @@
 //var funciones = require('./curso3');
 var cliente = require('./cliente');
+var cliente3 = require("./cliente2")
 
-var arreglo = cliente.b();
-var solonombres = cliente.c();
+let n_cliente = new cliente3();
+
+
+
+var datos = cliente3.cli2();
+console.log(datos);
+
+//var arreglo = cliente.b();
+//var solonombres = cliente.c();
 //console.log(arreglo);
-console.log(solonombres);
+//console.log(solonombres);
 
